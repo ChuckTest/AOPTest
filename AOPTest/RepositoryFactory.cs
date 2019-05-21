08e72982639fbf01294969444d9c9991a38863d8
Chuck Lu <chuck.lu@qq.com>
2019-05-21 14:20:08 +0800

    add repository factory

---
+++ AOPTest/RepositoryFactory.cs
@@ -0,0 +1,12 @@
+﻿namespace AOPTest
+{
+    public class RepositoryFactory
+    {
+        public static IRepository<T> Create<T>()
+        {
+            var repository = new Repository<T>();
+            var dynamicProxy = new DynamicProxy<IRepository<T>>(repository);
+            return dynamicProxy.GetTransparentProxy() as IRepository<T>;
+        }
+    }
+}
