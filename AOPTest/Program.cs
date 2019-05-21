a0cb1bbbf4c3413daf0888b291fb034b0d72e732
Chuck Lu <chuck.lu@qq.com>
2019-05-21 13:46:55 +0800

    Output of the Program with No Logging

--- AOPTest/Program.cs
+++ AOPTest/Program.cs
@@ -6,7 +6,20 @@ namespace AOPTest
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("***\r\n Begin program - no logging\r\n");
+            IRepository<Customer> customerRepository =
+                new Repository<Customer>();
+            var customer = new Customer
+            {
+                Id = 1,
+                Name = "Customer 1",
+                Address = "Address 1"
+            };
+            customerRepository.Add(customer);
+            customerRepository.Update(customer);
+            customerRepository.Delete(customer);
+            Console.WriteLine("\r\nEnd program - no logging\r\n***");
+            Console.ReadLine();
         }
     }
 }
